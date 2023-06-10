082c14d57c633116ef60e521aa45ecfcdd68279f
unknown <mm.88.mm128@gmail.com>
2023-06-11 01:54:02 +0300

    adding web

--- project/settings.py
+++ project/settings.py
@@ -55,12 +55,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 
    # 'corsheaders.middleware.CorsMiddleware',
 ]
@@ -146,6 +148,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "media",
 ]
 X_FRAME_OPTIONS ='SAMEORIGIN'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
